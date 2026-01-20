9d2aaa9d744989c442675d7a7aca2aeb63621230
Edmar D. O. Filho <87383421+Edmar8111@users.noreply.github.com>
2026-01-19 22:45:08 -0300

    Refactor ProductService to use kwargs in create_product

--- _developer/produto_service.py
+++ _developer/produto_service.py
@@ -3,23 +3,24 @@ import event_db
 
 
 class ProductService:
-    def __init__(self, repository=produto_repository):
-        self.repository=repository
+    def __init__(self):
+        self.repository=produto_repository
         self.eventlog=event_db.EventLog()
         pass
     def get_all_products(self):
         return self.repository.all_products()
 
-    def create_product(self, product_dict, owner_id):
+    def create_product(self, **kwargs):
         msg=self.repository.create_product(
-            user_id=owner_id, produto=product_dict["produto"], 
-            quantidade=product_dict["quantidade"],
-            valor=product_dict["valor"], peso=product_dict["peso"]
+            user_id=kwargs["owner_id"], produto=kwargs["product_dict"]["produto"], 
+            quantidade=kwargs["product_dict"]["quantidade"],
+            valor=kwargs["product_dict"]["valor"], peso=kwargs["product_dict"]["peso"]
         )
         self.eventlog.create_event(msg, "outbox")
 
         return msg
 
     def delete_product(self, id):
+
 
         return self.repository.delete_product(id)
