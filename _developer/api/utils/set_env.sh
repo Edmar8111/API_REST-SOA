f7df3706ecd9ca203c469931a65e60936190d551
Edmar <edmar@gestorsistemas.com>
2026-01-28 02:31:10 -0300

    modified:   _developer/api/utils/set_env.sh

--- _developer/api/utils/set_env.sh
+++ _developer/api/utils/set_env.sh
@@ -4,22 +4,14 @@
 # Torna as variáveis globais na sessão atual
 # ==========================================
 
-# ----------------------
-# MySQL
-# ----------------------
-export MYSQL_ROOT_PASS="root_pass"
+# Definição das variáveis de ambiente
+export MYSQL_ROOT_PASSWORD="root_pass"
 export MYSQL_DATABASE="app_db"
 export MYSQL_USER="app_user"
-export MYSQL_PASS="app_pass"
+export MYSQL_PASSWORD="app_pass"
 
-# ----------------------
-# App
-# ----------------------
 export DB_HOST="mysql"
 export DB_PORT="3306"
 
-# ----------------------
-# Feedback visual
-# ----------------------
 echo "Variáveis de ambiente carregadas com sucesso:"
 env | grep -E "MYSQL_|DB_"
